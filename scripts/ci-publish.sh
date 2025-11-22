6f82311e0ad41fa8e55689e8f4b897f3871de2b5
J. Simon Richard <jsimonrichard@gmail.com>
2025-11-22 11:17:53 -0500

    Fix ci-publish script error

--- scripts/ci-publish.sh
+++ scripts/ci-publish.sh
@@ -6,8 +6,9 @@ VERSION=$(jq -r .version package.json)
 set -e
 
 if npm view "${NAME}" versions --json | grep -q "\"${VERSION}\""; then
-    echo "Version ${VERSION} already published, skipping."
+    echo "Package ${NAME}@${VERSION} already published, skipping."
 else
-    bun pn pack --filename prosemark-package.tgz
+    echo "Publishing ${NAME}@${VERSION}..."
+    bun pm pack --filename prosemark-package.tgz
     npm publish prosemark-package.tgz
 fi
